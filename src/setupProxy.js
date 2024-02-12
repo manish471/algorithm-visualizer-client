51520dfa4a53ce3a4a0221860219c8ed88f2aeb9
MAChougule <manish.chougule@xecurify.com>
2024-02-12 12:43:12 +0530

    server proxy url updated

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api/',
     createProxyMiddleware({
-      target: 'http://localhost:3001',
+      target: 'https://algorithm-visualizer-server.vercel.app',
       changeOrigin: true,
     })
   );
